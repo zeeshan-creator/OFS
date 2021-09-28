eddb9d01555b9a4882c999d844e9929265fde4d6
ZEESHAN KHAN <zeekhan9753@outlook.com>
2021-09-28 17:48:57 +0500

    updated db.php

--- config/db.php
+++ config/db.php
@@ -2,12 +2,13 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'engroup');
-define('DB_PASSWORD', 'Bu#m(DqY4uSQ');
+define('DB_USERNAME', 'engroup_root');
+define('DB_PASSWORD', 'aeZ-hzEtsceX');
+define('DB_NAME', 'engroup_OFS');
 
 
 /* Attempt to connect to MySQL database */
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
 if ($conn === false) {
