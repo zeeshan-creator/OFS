918b924f7760b06d37d037e559e7bc4cd39cd516
ZEESHAN KHAN <zeekhan9753@outlook.com>
2021-09-28 18:14:47 +0500

    local & hosting MYSQL settings fiexd

--- config/db.php
+++ config/db.php
@@ -1,11 +1,16 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'engroup_root');
-define('DB_PASSWORD', 'aeZ-hzEtsceX');
-define('DB_NAME', 'engroup_OFS');
 
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'engroup_root');
+// define('DB_PASSWORD', 'aeZ-hzEtsceX');
+// define('DB_NAME', 'engroup_OFS');
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'engroup_OFS');
 
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
