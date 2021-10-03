4d12fb3d4a1467cb51a3da8f067d3c6b8780ae75
ZEESHAN KHAN <zeekhan9753@outlook.com>
2021-10-03 23:10:26 +0500

    turning on error reporting

--- config/db.php
+++ config/db.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
