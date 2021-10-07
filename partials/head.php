d97bb056c7f090cfe5d57cdbecc01fd30544fe76
ZEESHAN KHAN <zeekhan9753@outlook.com>
2021-10-07 18:27:31 +0500

    .

--- partials/head.php
+++ partials/head.php
@@ -21,22 +21,24 @@ ob_end_flush();
   <!--   Core JS Files   -->
   <script src="./assets/js/core/jquery.min.js"></script>
 
-
-
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
   <!-- Nucleo Icons -->
   <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+
   <!-- CSS Files -->
   <link href="./assets/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="./assets/demo/demo.css" rel="stylesheet" />
 
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <!-- <link href="./assets/demo/demo.css" rel="stylesheet" /> -->
+
+  <!-- DataTables Script & CSS Links -->
   <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
   <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 
-  <!-- <style>
+  <style>
     /* Chrome, Safari, Edge, Opera */
     input::-webkit-outer-spin-button,
     input::-webkit-inner-spin-button {
@@ -48,6 +50,6 @@ ob_end_flush();
     input[type=number] {
       -moz-appearance: textfield;
     }
-  </style> -->
+  </style>
 
 </head>
