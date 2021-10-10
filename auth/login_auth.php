281cfe1f9580f64c4aa18dbff59b8ba1dd12191f
ZEESHAN KHAN <zeekhan9753@outlook.com>
2021-10-10 14:58:42 +0500

    .

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -3,7 +3,8 @@ ob_start();
 session_start();
 
 if (!isset($_SESSION['name'])) {
-   header("Location: login");
+   // header("Location: login");
+   echo '<script>window.location.href = "login";</script>';
    exit();
 }
 ob_end_flush();
