96895523b6df33f88be83e2ba8ea100b3bb7c506
ZEESHAN KHAN <zeekhan9753@outlook.com>
2021-11-08 19:07:27 +0500

    .

--- partials/head.php
+++ partials/head.php
@@ -6,6 +6,7 @@
   <title>OFS</title>
 
   <!-- Google Font: Source Sans Pro -->
+  <link rel="shortcut icon" href="docs/assets/img/AdminLTELogo.png" type="image/x-icon">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
