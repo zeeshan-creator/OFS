34ae5a14a10e2255c6ad128a5b7c871e7fa3d89f
ZEESHAN KHAN <zeekhan9753@outlook.com>
2021-12-08 19:28:55 +0500

    .

--- POS.php
+++ POS.php
@@ -18,11 +18,11 @@ if (isset($_POST['action']) && $_POST['action'] == "remove") {
     foreach ($_SESSION["shopping_cart"] as $id => $value) {
       if ($_POST["key"] == $id) {
         unset($_SESSION["shopping_cart"][$id]);
-        echo "<script>window.location.href = 'pos';</script>";
+        echo "<script>window.location.href = 'POS';</script>";
       }
       if (empty($_SESSION["shopping_cart"])) {
         unset($_SESSION["shopping_cart"]);
-        echo "<script>window.location.href = 'pos';</script>";
+        echo "<script>window.location.href = 'POS';</script>";
       }
     }
   }
