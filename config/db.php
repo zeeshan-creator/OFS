740842b529315d42c96a13b6174791cfe2a508fd
ZEESHAN KHAN <zeekhan9753@outlook.com>
2021-12-16 18:24:36 +0500

    .

--- config/db.php
+++ config/db.php
@@ -9,15 +9,15 @@ date_default_timezone_set("Asia/Karachi");
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'engroup_root');
-// define('DB_PASSWORD', 'aeZ-hzEtsceX');
-// define('DB_NAME', 'engroup_OFS');
-
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'engroup_ofs');
+define('DB_USERNAME', 'engroup_root');
+define('DB_PASSWORD', 'aeZ-hzEtsceX');
+define('DB_NAME', 'engroup_OFS');
+
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'engroup_ofs');
 
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
